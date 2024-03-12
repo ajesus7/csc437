dd26e975c43d0f9fa2e947075e07cda1ba4bde4b
Aidan Nesbitt <aidannesbitt20@gmail.com>
2024-03-11 21:24:16 -0700

    created ts-models

---
+++ packages/ts-models/src/profile.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+  userid: string;
+  name: string;
+  timezone: string;
+  spotify: boolean;
+  musicTaste?: string;
+  profileImage?: string;
+  profileDescription?: string;
+  bio: string;
+}
